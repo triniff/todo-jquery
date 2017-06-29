36a98b96d76e6dfd23a8144a66662249fc79663e
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-06-29 09:19:01 -0400

    agregando boton eliminar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
-	$(".agregar").click(function(){
-		var tarea = $("#tarea").val();
-		var id = "test5";
-		$(".tareas-agregadas").append('<div><input type="checkbox" class="check"/><label>'+ tarea +'</label></div>'); //agregando elemento a la lista
+ 	$.fn.enterKey = function (fnc) { //funcion para cuando hagas enter se accione lo demas
+	    return this.each(function () {
+	        $(this).keypress(function (ev) {
+	            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+	            if (keycode == '13') {
+	                fnc.call(this, ev);
+	            }
+	        })
+	    })
+	}
+	$("#tarea").enterKey(function (e) {
+		$(this).trigger("enterEvent");
+		e.preventDefault();
+    	var tarea = $("#tarea").val();
+		if (tarea == "") {
+			alert("Tienes que agregar una tarea");
+		}else{
+			$(".tareas-agregadas").append('<div><input type="checkbox" class="check"/><label>'+ tarea +'</label> <button class="remove btn" > Eliminar </button> </div>'); //agregando elemento a la lista
 			$("#tarea").val(""); //limpiando input
-
-		/*	var container = $('#tareas-agregadas');
-			var inputs = container.find('input');
-		   	var id = inputs.length+1;
-
-		   $('#tareas-agregadas').append('<input />', { type: 'checkbox', id: 'cb'+id, value:tarea });
-		   $('#tareas-agregadas').append('<label />', { 'for': 'cb'+id, text: tarea });*/
+		}
+		$(".remove").click(function(){
+			$(this).parent('div').remove();
+		})
 	})
+	
+
 })
